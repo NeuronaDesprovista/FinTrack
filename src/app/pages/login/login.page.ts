c57a6a709eba6b1666a3c32855d5fb42e92ea8a7
CITT <arno.gonzalez@duocuc.cl>
2024-10-28 17:04:29 -0300

    Proyecto inicial ionic

---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  username:string = "";
+  password:string = "";
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+  recoverPassword(){
+
+  }
+
+  login(){
+    
+  }
+
+}
